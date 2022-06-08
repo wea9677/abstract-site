29d2c5014dd8589d918dd453b32afdc6628d30f2
wea9677 <72002228+wea9677@users.noreply.github.com>
2022-06-08 10:27:59 +0900

    테스트 코드 과제

--- src/index.js
+++ src/index.js
@@ -1,10 +1,80 @@
-class Site {}
+class Site {
+    constructor(){
+        this.boards=[]
+    }
+    //동일한 이름의 board를 추가
+    addBoard(board) {
+        if (this.boards.find((boardInfo) => boardInfo.name === board.name)){
+            throw Error()
+        }
+        board.myBoard = true
+        this.boards.push(board)
+    }
+    findBoardByName(name) {
+        return this.boards.find((board) => board.name = name)
+    }
+  
+}
 
-class Board {}
+class Board {
+    
+    constructor(name) {
+        if (name === null || name === "") {
+            throw Error()
+        }
+        this.name = name
+        this.myBoard = false
+        this.articles = []
+    }
+    publish(article) {
+        if (this.myBoard == false) {
+            throw Error()
+        }
+        article.id = `${this.name}-${Math.random()}`
+        article.createdDate = new Date().toISOString()
+        this.articles.push(article)
+    }
+    getAllArticles() {
+        return this.articles
+    }
 
-class Article {}
+}
 
-class Comment {}
+class Article {
+    constructor(article) {
+        const { subject, content, author } = article
+        if (subject === null || subject === "" || content === null || content === "" || author === null || author === "") {
+            throw Error()
+        }
+        this.subject = subject
+        this.content = content
+        this.author = author
+        this.comments = []
+    }
+    reply(comment) {
+        if (!this.id) {
+            throw Error()
+        }
+        comment.createdDate = new Date().toISOString()
+        this.comments.push(comment)
+    }
+    getAllComments() {
+        return this.comments
+    }
+
+}
+
+class Comment {
+
+    constructor(comment) {
+        const { content, author } = comment
+        if (content === null || content === "" || author === null || author === "") {
+            throw Error()
+        }
+        this.content = content
+        this.author = author
+    }
+}
 
 module.exports = {
     Site,
